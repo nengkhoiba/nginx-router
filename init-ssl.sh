6082b9014252f8dd3f63cb6d1a072e7b2420b87b
Nengkhoiba Chungkham <nengkhoiba@Nengkhoibas-MacBook-Pro.local>
2024-06-14 15:14:16 +0530

    init

---
+++ init-ssl.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+echo "### Creating nginx proxy router"
+read -p "Enter your source domain FQDN: " domain
+
+read -p "Enter your destination domain FQDN: " destination
+
+read -p "Enter your email for sending ssl information: " email
+
+
+domains=($domain)
+email=$email
+data_path="./certbot"
+nginx_path="./nginx/conf"
+certbot_path="/var/www/certbot"
+
+if [ -d "$data_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+# Create the necessary folders
+mkdir -p "$data_path/www"
+mkdir -p "$data_path/conf"
+
+
+echo "### Deleting existing certificates for $domains ..."
+rm -rf "$nginx_path/default.conf"
+rm -rf "$nginx_path/default-ssl.conf"
+
+mkdir -p "$nginx_path"
+cat > "$nginx_path/default.conf" << EOF
+server {
+    listen 80;
+    server_name $domain;
+
+    location /.well-known/acme-challenge/ {
+        root /var/www/certbot;
+    }
+
+    location / {
+       return 301 https://$destination$request_uri;
+    }
+}
+
+EOF
+echo "### Starting nginx ..."
+docker-compose up --force-recreate -d nginx
+
+echo "### Deleting existing certificates for $domains ..."
+rm -rf "$data_path/conf/live/$domains"
+rm -rf "$data_path/conf/archive/$domains"
+rm -rf "$data_path/conf/renewal/$domains.conf"
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+email_arg="--email $email"
+if [ -z "$email" ]; then
+  email_arg="--register-unsafely-without-email"
+fi
+
+staging_arg=""
+if [ $staging != "0" ]; then
+  staging_arg="--staging"
+fi
+
+docker-compose run --rm --entrypoint "\
+  certbot certonly --webroot -w $certbot_path \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --agree-tos \
+    --force-renewal" certbot
+
+echo "### Stopping nginx ..."
+docker-compose down
+
+echo "### Creating Nginx configuration for SSL ..."
+mkdir -p "$nginx_path"
+cat > "$nginx_path/default.conf" << EOF
+server {
+    listen 80;
+    server_name $domain;
+
+    location /.well-known/acme-challenge/ {
+        root /var/www/certbot;
+    }
+
+    location / {
+        return 301 https://$destination$request_uri;
+    }
+}
+server {
+    listen 443 default_server ssl http2;
+    listen [::]:443 ssl http2;
+
+    server_name $domain;
+
+    ssl_certificate /etc/nginx/ssl/live/apeiro.digital/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/live/apeiro.digital/privkey.pem;
+    
+    location / {
+        return 301 https://$destination$request_uri;
+    }
+}
+EOF
+
+
+echo "### Reloading nginx with new SSL configuration ..."
+docker-compose up --force-recreate -d nginx
+
+echo "### SSL setup completed successfully."
---
+++ readme.md
@@ -0,0 +1,121 @@
+# Nginx Router
+
+---
+
+This is a SSL enable nginx router that you can comtomize to run on a docker container.
+SSL is generated using LetsEncrypt.
+
+## Make Executable
+
+### Linux
+
+```sh
+sudo chmod +x init-ssl.sh
+```
+
+### Initiate the router
+
+```sh
+sudo ./init-ssl.sh
+```
+
+### Inputs
+
+```
+Enter your source domain FQDN: example.com // Domain to enable ssl and accept the traffic to the router
+Enter your destination domain FQDN: www.example2.com // Domain with you want to redirect to
+Enter your email for sending ssl information: example@email.com //Email to get alert about ssl expiry
+
+```
+
+## Modification to turn into proxy server
+
+Open `init-ssl.sh`
+
+Look for this code
+
+```sh
+cat > "$nginx_path/default.conf" << EOF
+server {
+    listen 80;
+    server_name $domain;
+
+    location /.well-known/acme-challenge/ {
+        root /var/www/certbot;
+    }
+
+    location / {
+        return 301 https://$destination$request_uri;
+    }
+}
+server {
+    listen 443 default_server ssl http2;
+    listen [::]:443 ssl http2;
+
+    server_name $domain;
+
+    ssl_certificate /etc/nginx/ssl/live/apeiro.digital/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/live/apeiro.digital/privkey.pem;
+
+    location / {
+        return 301 https://$destination$request_uri;
+    }
+}
+EOF
+```
+
+### Hosting node app as reverse proxy
+
+Change _location_ to
+
+```sh
+location / {
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Host $http_host;
+      proxy_set_header X-NginX-Proxy true;
+
+      proxy_pass http://127.0.0.1:3000/; # your host
+      proxy_redirect off;
+    }
+```
+
+### Hosting React Build
+
+```sh
+
+server {
+    listen 80;
+    server_name $domain;
+
+    location /.well-known/acme-challenge/ {
+        root /var/www/certbot;
+    }
+
+    root /var/www/html;  # Change this path to the location of your build files
+    location / {
+        try_files $uri /index.html;
+    }
+}
+server {
+    listen 443 default_server ssl http2;
+    listen [::]:443 ssl http2;
+
+    server_name $domain;
+
+    ssl_certificate /etc/nginx/ssl/live/apeiro.digital/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/live/apeiro.digital/privkey.pem;
+
+    root /var/www/html;  # Change this path to the location of your build files
+    location / {
+        try_files $uri /index.html;
+    }
+}
+
+
+
+```
+
+## License
+
+See the [LICENSE](LICENSE.md) file for license rights and limitations (MIT).
